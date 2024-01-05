a98934a7f5d1b73189d6e3bff1d6d3885259cf0b
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-01-05 10:58:38 +0400

    v05.01.2024

--- dev/data.py
+++ dev/data.py
@@ -14717,7 +14717,45 @@ m = [
 ]
 print(linalg.det(m))
 ```
-"""
+""",
+            "Минимальное максимальное число матрицы cpp": r"""
+```cpp
+#file maxminmatrix.cpp
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int rows = 3;
+    int cols = 4;
+    srand(time(NULL));
+    vector<vector<int>> matrix(rows, vector<int>(cols, 0));
+    for (int i = 0; i < rows; ++i) {
+        cout << "|";
+        for (int j = 0; j < cols; ++j) {
+            int number = 1 + rand() % 999;
+            matrix[i][j] = number;
+            cout << " " << setw(3) << setfill(' ') << number << " |";
+        }
+        cout << "\n";
+    }
+    int max = 0;
+    int min = 999;
+    for (auto r : matrix) {
+        for (auto c : r) {
+            if (c > max) max = c;
+            if (c < min) min = c;
+        }
+    }
+    cout << "max = " << max << "\n" << "min = " << min << "\n";
+    return 0;
+}
+```
+""",
         },
         "Пример этой кнопки": """
 ```
