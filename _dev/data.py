cdc938e5f33835445aa08a8d41a7e419f073dacc
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-10-03 02:07:52 +0400

    update cheat sheet

--- _dev/data.py
+++ _dev/data.py
@@ -25536,6 +25536,27 @@ class Dict:
 
 """,
         },
+        "Chrome Dino": """
+[chrome://dino/](chrome://dino/)
+
+```javascript
+Runner.instance_.setSpeed(1000)
+```
+```javascript
+Runner.prototype.gameOver = function(){}
+```
+
+Setting the current score
+Let’s set the score to 12345. You can set any other score less than 99999. The current score is reset on game over.
+```javascript
+Runner.instance_.distanceRan = 12345 / Runner.instance_.distanceMeter.config.COEFFICIENT
+```
+
+You can control how high the dino jumps by using the below function. Adjust the value as necessary.
+```javascript
+Runner.instance_.tRex.setJumpVelocity(10)
+```
+""",
         "Alt Symbols": """
 
 | Символ | Код  |
