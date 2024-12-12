dd2999ecb8d535b8e3a9b5a66677e24b03fa5333
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-12-12 23:53:45 +0400

    update

--- _dev/data.py
+++ _dev/data.py
@@ -21683,6 +21683,11 @@ public class Main {
             "index": """
 [https://devhints.io/kotlin](https://devhints.io/kotlin)
 """,
+            "Android": """
+[https://youtube.com/playlist?list=PLDyJYA6aTY1nZqYprT1PKtDFthBcZWAMZ](https://youtube.com/playlist?list=PLDyJYA6aTY1nZqYprT1PKtDFthBcZWAMZ)
+[https://itproger.com/course/java-android/3](https://itproger.com/course/java-android/3)
+![activity_lifecycle.png](Languages/Kotlin/activity_lifecycle.png)
+""",
         },
         "SQL": {
             "SQLite3": {
---
+++ cheatsheet/Languages/Kotlin/Android.md
@@ -0,0 +1,3 @@
+<p><a href="https://youtube.com/playlist?list=PLDyJYA6aTY1nZqYprT1PKtDFthBcZWAMZ">youtube.com/playlist</a>
+<a href="https://itproger.com/course/java-android/3">https://itproger.com/course/java-android/3</a>
+<img alt="activity_lifecycle.png" src="Languages/Kotlin/activity_lifecycle.png" /></p>
