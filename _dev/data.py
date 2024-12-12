fbfab3d21e3e6db88bad541d15014990a50acda3
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-12-13 00:35:27 +0400

    update

--- _dev/data.py
+++ _dev/data.py
@@ -27512,25 +27512,25 @@ storage-method: MariaDB
 """,
                 },
             },
-            "Colors": """
-| color name   |                                                                         | color | chat | hex code  | rgb           | motd       | decimal    |
-|--------------|-------------------------------------------------------------------------|:-----:|:----:|-----------|---------------|------------|------------|
-| dark_red     | <div style="background-color: #AA0000;width: 50px;height: 15px;"></div> | `&4`  | `§4` | #`AA0000` | `170 0 0`     | `\\u00A74` | `11141120` |
-| red          | <div style="background-color: #FE5555;width: 50px;height: 15px;"></div> | `&c`  | `§c` | #`FF5555` | `255 85 85`   | `\\u00A7c` | `16733525` |
-| gold         | <div style="background-color: #FFAA00;width: 50px;height: 15px;"></div> | `&6`  | `§6` | #`FFAA00` | `255 170 0`   | `\\u00A76` | `16755200` |
-| yellow       | <div style="background-color: #FFFF55;width: 50px;height: 15px;"></div> | `&e`  | `§e` | #`FFFF55` | `255 255 85`  | `\\u00A7e` | `16777045` |
-| green        | <div style="background-color: #55FF55;width: 50px;height: 15px;"></div> | `&a`  | `§a` | #`55FF55` | `0 170 0`     | `\\u00A7a` | `5635925`  |
-| dark_green   | <div style="background-color: #00AA00;width: 50px;height: 15px;"></div> | `&2`  | `§2` | #`00AA00` | `85 255 85`   | `\\u00A72` | `43520`    |
-| aqua         | <div style="background-color: #55FFFF;width: 50px;height: 15px;"></div> | `&b`  | `§b` | #`55FFFF` | `85 255 255`  | `\\u00A7b` | `5636095`  |
-| dark_aqua    | <div style="background-color: #00AAAA;width: 50px;height: 15px;"></div> | `&3`  | `§3` | #`00AAAA` | `0 170 170`   | `\\u00A73` | `43690`    |
-| blue         | <div style="background-color: #5555FF;width: 50px;height: 15px;"></div> | `&9`  | `§9` | #`5555FF` | `0 0 170`     | `\\u00A79` | `5592575`  |
-| dark_blue    | <div style="background-color: #0000AA;width: 50px;height: 15px;"></div> | `&1`  | `§1` | #`0000AA` | `85 85 255`   | `\\u00A71` | `170`      |
-| light_purple | <div style="background-color: #FF55FF;width: 50px;height: 15px;"></div> | `&d`  | `§d` | #`FF55FF` | `255 85 255`  | `\\u00A7d` | `16733695` |
-| dark_purple  | <div style="background-color: #AA00AA;width: 50px;height: 15px;"></div> | `&5`  | `§5` | #`AA00AA` | `170 0 170`   | `\\u00A75` | `11141290` |
-| white        | <div style="background-color: #FFFFFF;width: 50px;height: 15px;"></div> | `&f`  | `§f` | #`FFFFFF` | `255 255 255` | `\\u00A7f` | `16777215` |
-| gray         | <div style="background-color: #AAAAAA;width: 50px;height: 15px;"></div> | `&7`  | `§7` | #`AAAAAA` | `170 170 170` | `\\u00A77` | `11184810` |
-| dark_gray    | <div style="background-color: #555555;width: 50px;height: 15px;"></div> | `&8`  | `§8` | #`555555` | `85 85 85`    | `\\u00A78` | `5592405`  |
-| black        | <div style="background-color: #000000;width: 50px;height: 15px;"></div> | `&0`  | `§0` | #`000000` | `0 0 0`       | `\\u00A70` | `0`        |
+            "Colors": r"""
+| color name   |                                                                         | color | chat | hex code  | rgb           | motd      | decimal    |
+|--------------|-------------------------------------------------------------------------|:-----:|:----:|-----------|---------------|-----------|------------|
+| dark_red     | <div style="background-color: #AA0000;width: 50px;height: 15px;"></div> | `&4`  | `§4` | #`AA0000` | `170 0 0`     | `\u00A74` | `11141120` |
+| red          | <div style="background-color: #FE5555;width: 50px;height: 15px;"></div> | `&c`  | `§c` | #`FF5555` | `255 85 85`   | `\u00A7c` | `16733525` |
+| gold         | <div style="background-color: #FFAA00;width: 50px;height: 15px;"></div> | `&6`  | `§6` | #`FFAA00` | `255 170 0`   | `\u00A76` | `16755200` |
+| yellow       | <div style="background-color: #FFFF55;width: 50px;height: 15px;"></div> | `&e`  | `§e` | #`FFFF55` | `255 255 85`  | `\u00A7e` | `16777045` |
+| green        | <div style="background-color: #55FF55;width: 50px;height: 15px;"></div> | `&a`  | `§a` | #`55FF55` | `0 170 0`     | `\u00A7a` | `5635925`  |
+| dark_green   | <div style="background-color: #00AA00;width: 50px;height: 15px;"></div> | `&2`  | `§2` | #`00AA00` | `85 255 85`   | `\u00A72` | `43520`    |
+| aqua         | <div style="background-color: #55FFFF;width: 50px;height: 15px;"></div> | `&b`  | `§b` | #`55FFFF` | `85 255 255`  | `\u00A7b` | `5636095`  |
+| dark_aqua    | <div style="background-color: #00AAAA;width: 50px;height: 15px;"></div> | `&3`  | `§3` | #`00AAAA` | `0 170 170`   | `\u00A73` | `43690`    |
+| blue         | <div style="background-color: #5555FF;width: 50px;height: 15px;"></div> | `&9`  | `§9` | #`5555FF` | `0 0 170`     | `\u00A79` | `5592575`  |
+| dark_blue    | <div style="background-color: #0000AA;width: 50px;height: 15px;"></div> | `&1`  | `§1` | #`0000AA` | `85 85 255`   | `\u00A71` | `170`      |
+| light_purple | <div style="background-color: #FF55FF;width: 50px;height: 15px;"></div> | `&d`  | `§d` | #`FF55FF` | `255 85 255`  | `\u00A7d` | `16733695` |
+| dark_purple  | <div style="background-color: #AA00AA;width: 50px;height: 15px;"></div> | `&5`  | `§5` | #`AA00AA` | `170 0 170`   | `\u00A75` | `11141290` |
+| white        | <div style="background-color: #FFFFFF;width: 50px;height: 15px;"></div> | `&f`  | `§f` | #`FFFFFF` | `255 255 255` | `\u00A7f` | `16777215` |
+| gray         | <div style="background-color: #AAAAAA;width: 50px;height: 15px;"></div> | `&7`  | `§7` | #`AAAAAA` | `170 170 170` | `\u00A77` | `11184810` |
+| dark_gray    | <div style="background-color: #555555;width: 50px;height: 15px;"></div> | `&8`  | `§8` | #`555555` | `85 85 85`    | `\u00A78` | `5592405`  |
+| black        | <div style="background-color: #000000;width: 50px;height: 15px;"></div> | `&0`  | `§0` | #`000000` | `0 0 0`       | `\u00A70` | `0`        |
 
 |            |    |
 |------------|----|
@@ -27540,14 +27540,14 @@ storage-method: MariaDB
 | hex code   | paint color and web css colors
 | decimal    | used for nbt data tags, setting potion and leather armour colors
 
-| style         | chat   | motd       |
-|---------------|--------|------------|
-| Obfuscated    | `§k`   | `\\u00A7k` |
-| Bold          | `§l`   | `\\u00A7l` |
-| Strikethrough | `§m`   | `\\u00A7m` |
-| Underline     | `§n`   | `\\u00A7n` |
-| Italic        | `§o`   | `\\u00A7o` |
-| Reset         | `§r`   | `\\u00A7r` |
+| style         | chat   | motd      |
+|---------------|--------|-----------|
+| Obfuscated    | `§k`   | `\u00A7k` |
+| Bold          | `§l`   | `\u00A7l` |
+| Strikethrough | `§m`   | `\u00A7m` |
+| Underline     | `§n`   | `\u00A7n` |
+| Italic        | `§o`   | `\u00A7o` |
+| Reset         | `§r`   | `\u00A7r` |
 
 ![colors.png](Other/Minecraft/colors.png)
 """,
