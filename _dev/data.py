157078f05e5f40aee57539d97901bf7821482e15
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-12-13 00:27:34 +0400

    update

--- _dev/data.py
+++ _dev/data.py
@@ -27555,6 +27555,59 @@ storage-method: MariaDB
 [plotz.co.uk](https://www.plotz.co.uk/)
 """
         },
+        "JetBrains IDE": {
+            "settings": """
+# Keymap
+|                                                                                     |                    |
+|-------------------------------------------------------------------------------------|--------------------|
+| `Editor Actions` `Delete Line`                                                      |                    |
+| `Editor Actions` `Extend Selection`                                                 | `Ctrl`+`S`         |
+| `Editor Actions` `Column Selection Mode`                                            | `Alt`+`S`          |
+| `Editor Actions` `Add or Remove Caret`                                              | `Alt`+`Click`      |
+| `Main Menu` `File` `File Open Actions` `New…`                                       | `Ctrl`+`T`         |
+| `Main Menu` `File` `File Open Actions` `Scratch File`                               | `Ctrl`+`Shift`+`T` |
+| `Main Menu` `Edit` `Redo`                                                           | `Ctrl`+`Y`         |
+| `Main Menu` `Edit` `Find` `Find Next / Move to Next Occurrence`                     | `Alt`+`.`          |
+| `Main Menu` `Edit` `Find` `Find Previous / Move to Previous Occurrence`             | `Alt`+`Comma`      |
+| `Main Menu` `Edit` `Find` `Select All Occurrences`                                  | `Alt`+`M`          |
+| `Main Menu` `Edit` `Column Selection Mode`                                          | `Alt`+`S`          |
+| `Main Menu` `Edit` `Editor Select Actions` `Select Word Actions` `Extend Selection` | `Ctrl`+`S`         |
+| `Main Menu` `View` `Appearance` `Toggle Presentation mode`                          | `Alt`+`P`          |
+| `Main Menu` `Navigate` `Goto by Reference Actions` `Go to Test`                     |                    |
+| `Main Menu` `Refactor` `Rename…`                                                    | `Alt`+`R`          |
+| `Main Menu` `Run` `Run/Debug` `Run`                                                 | `Alt`+`X`          |
+| `Main Menu` `Run` `Run/Debug` `Debug`                                               | `Alt`+`Z`          |
+| `Main Menu` `Run` `Run…`                                                            |                    |
+| `Main Menu` `Run` `Stop`                                                            | `Alt`+`C`          |
+| `Main Menu` `Git` `VCS Group` `Update Project`                                      |                    |
+| `Main Menu` `Git` `Update Project`                                                  |                    |
+| `Main Menu` `Window` `Active Tool Window` `Close Active Tab`                        | `Ctrl`+`W`         |
+| `Main Menu` `Window` `Editor Tabs` `Editor Close Actions` `Close Tab`               | `Ctrl`+`W`         |
+| `Tool Windows` `BookMarks`                                                          |                    |
+| `Tool Windows` `Commit`                                                             |                    |
+| `Tool Windows` `Debug`                                                              |                    |
+| `Tool Windows` `Find`                                                               |                    |
+| `Tool Windows` `Problems`                                                           |                    |
+| `Tool Windows` `Project`                                                            |                    |
+| `Tool Windows` `Run`                                                                |                    |
+| `Tool Windows` `Services`                                                           |                    |
+| `Tool Windows` `Structure`                                                          |                    |
+| `Tool Windows` `Git`                                                                |                    |
+| `Version Control Systems` `Diff & Merge/Next Difference`                            | `Alt`+`.`          |
+| `Version Control Systems` `Diff & Merge/Previous Difference`                        | `Alt`+`Comma`      |
+| `Version Control Systems` `Amend Commit`                                            | `Ctrl`+`K`         |
+| `Version Control Systems` `Update Project`                                          |                    |
+
+# Editor
+## Font
+|                    |                                 |
+|--------------------|---------------------------------|
+| `Size`             | `10.0`                          |
+| `Line`             | `1.0`                           |
+| `Enable Ligatures` | <input type="checkbox" checked> |
+
+""",
+        },
         "ChatGPT": {
             "Шпаргалки": """
 Обычная шпаргалка
