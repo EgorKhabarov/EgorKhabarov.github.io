c984469bc5e67a596273b11694f38f13637f29ff
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-09-02 00:38:45 +0400

    update

--- resources/script.js
+++ resources/script.js
@@ -1,2 +1,2 @@
-footer.textContent = footer.textContent.replace("<year>", new Date().getFullYear());
+footer.textContent = `© ${new Date().getFullYear()} Egor Khabarov - egorkhabarov.github.io`;
 footer_script.remove();
