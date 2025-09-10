0c09097353bf5a4aac3bf638e47a217aa355943a
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-09-10 12:27:23 +0400

    update projects/pet/RunningTextGifGenerator/sandbox

--- projects/pet/RunningTextGifGenerator/resources/sandbox/worker.js
+++ projects/pet/RunningTextGifGenerator/resources/sandbox/worker.js
@@ -12,7 +12,6 @@ function stop_text_spinner() {
     self.postMessage({type: "log", text: " Done\n"});
 }
 
-
 // worker.js
 const pyodideReadyPromise = (async () => {
     self.postMessage({type: "log_clean"});
@@ -31,19 +30,6 @@ const pyodideReadyPromise = (async () => {
     pyodide.FS.writeFile("/fonts/Monocraft.otf", bytes);
     stop_text_spinner();
 
-    // function listdir(currentPath = "/") {
-    //     const entries = pyodide.FS.readdir(currentPath);
-    //     let results = [];
-    //     for (const entry of entries) {
-    //         if (entry === "." || entry === "..") continue;
-    //         const fullPath = currentPath + (currentPath === "/" ? "" : "/") + entry;
-    //         results.push(fullPath);
-    //     }
-    //     console.log(`"${currentPath}"`, results);
-    // }
-    // listdir();
-    // listdir("/fonts");
-
     self.send_log = (txt) => {self.postMessage({type: "log", text: txt});}
     self.send_err = (txt) => {self.postMessage({type: "err", text: txt});}
     self.postMessage({type: "log", text: "Ready!\n"});
@@ -52,18 +38,12 @@ const pyodideReadyPromise = (async () => {
 })();
 
 
-
-
-self.onmessage = async (event) => {
-    const {type, code} = event.data;
-    if (type !== "run") return;
-
-    const pyodide = await pyodideReadyPromise;
-
+    start_text_spinner("Loading code... ");
     try {
-        // оборачиваем код для перехвата stdout/stderr
-        const wrapped = `
+        const code = `
 import sys
+
+import js
 from js import send_log, send_err
 
 class JsStdout:
@@ -82,10 +62,59 @@ sys.stdout = JsStdout()
 sys.stderr = JsStderr()
 
 ${code}
+${await (await fetch("/projects/pet/RunningTextGifGenerator/resources/sandbox/gif.py")).text()}
+${await (await fetch("/projects/pet/RunningTextGifGenerator/resources/sandbox/run_gif.py")).text()}
+
+
+def print_progress_bar(x, y, name, start):
+    if not x % 3 or x == y:
+        return __print_progress_bar__(x, y, name, start)
+
+
+__global_color_config = {
+    "color_border": "#000000",
+    "color_background": "#222222",
+    "color_glare": "#666666",
+    "color_pixel_off_light": "#880000",
+    "color_pixel_off_dark": "#660000",
+    "color_pixel_on_light": "#FF6666",
+    "color_pixel_on_dark": "#FF0000",
+}
 `;
         let result = await pyodide.runPythonAsync(wrapped);
+        await pyodide.runPythonAsync(code);
+        stop_text_spinner();
+    } catch (err) {
+        self.postMessage({type: "stop_spinner"});
+        self.postMessage({type: "log", text: "\n"+err.message});
+        throw err;
+    }
+
+    self.postMessage({type: "log", text: "Ready!\n"});
+    self.postMessage({type: "log_clean"});
+
+    return pyodide;
+})();
+
+
+
+
+self.onmessage = async (event) => {
+    const {type, code} = event.data;
+
+    if (!["run", "stop"].includes(type)) {
+        return;
+    }
+
+    const pyodide = await pyodideReadyPromise;
+
+    self.postMessage({type: "gen_start"});
+
+    try {
+        let result = await pyodide.runPythonAsync(code);
         self.postMessage({type: "done", result});
     } catch (err) {
-        self.postMessage({type: "error", error: err.message});
+        self.postMessage({type: "log", text: err.message});
     }
+    self.postMessage({type: "gen_stop"});
 };
