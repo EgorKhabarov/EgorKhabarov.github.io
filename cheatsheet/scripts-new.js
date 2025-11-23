e74425826864a8324d5d21962b9096dba0089b33
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-11-23 22:26:54 +0400

    updated

--- cheatsheet/scripts-new.js
+++ cheatsheet/scripts-new.js
@@ -272,6 +272,19 @@ settingsBackdrop.addEventListener("click", () => toggleSettings(false));
 
 
 
+function load_cheatsheet(url) {
+    console.log(`load_cheatsheet "${url}.html"`)
+    fetch(url)
+        .then(response => response.text())
+        .then(text => {
+            cheatsheet_field.innerHTML = text;
+            processingCheatSheet();
+            cheatsheet_field.scrollTo(0, 0);
+        });
+}
+
+
+
 
 
 
@@ -280,9 +293,9 @@ settingsBackdrop.addEventListener("click", () => toggleSettings(false));
     /*
     fetch("cheatsheet_resources/buttons_new.html")
         .then(response => response.text())
-        .then(data => document.getElementById("folder_list").innerHTML = data);
+        .then(data => folderList.innerHTML = data);
     */
-    css_colors = {
+    const css_colors = {
         "default": ' style="color: var(--color-file-default);"',
         "yellow":  ' style="color: var(--color-file-yellow);"',
         "green":   ' style="color: var(--color-file-green);"',
@@ -323,8 +336,8 @@ settingsBackdrop.addEventListener("click", () => toggleSettings(false));
         const result_color = (color in css_colors) ? css_colors[color] : "";
         return `<svg viewBox="0 0 24 24"${result_color}><use href="#icon_file"/></svg>`;
     }
-    function formatCheatsheetButton(title, svg) {
-        return `<div class="tree_item file" title="${title}">${svg}<span class="label">${title}</span></div>`;
+    function formatCheatsheetButton(title, svg, vpath) {
+        return `<div class="tree_item file" title="${title}" data-vpath="${vpath}">${svg}<span class="label">${title}</span></div>`;
     }
     function formatFolderButton(title, svg, buttons_folder_data) {
         return `<div class="tree_group"><div class="tree_item folder" data-state="closed" title="{title}">${svg}<span class="label">${title}</span></div><div class="tree_children hidden">${buttons_folder_data}</div></div>`;
@@ -370,7 +383,7 @@ settingsBackdrop.addEventListener("click", () => toggleSettings(false));
                 const vpath = `${directoryE}/${key}`.replace(/^\/+/, "");
                 const svg = formatTagSvg(currentMetadata.color || "default");
                 const tags = currentMetadata.tags;
-                textList.push(formatCheatsheetButton(title, svg));
+                textList.push(formatCheatsheetButton(title, svg, vpath));
             }
         }
         return [textList.join(""), directory];
@@ -379,10 +392,32 @@ settingsBackdrop.addEventListener("click", () => toggleSettings(false));
     fetch("index.json")
         .then(response => response.json())
         .then(json_data => {
-            document.getElementById("folder_list").innerHTML = generateButtons(json_data)[0];
-    });
+            folderList.innerHTML = generateButtons(json_data)[0];
+            setup_folder_list_buttons();
+        });
 })();
-
+function setup_folder_list_buttons() {
+    folderList.querySelectorAll(".file").forEach(e => {
+        //e.click();
+        e.addEventListener("click", function(event) {
+            // const textList = [e.textContent];//.parentElement.firstElementChild.textContent
+            // while (e.parentElement !== folderList) {
+            //     console.log(e);
+            //     textList.push(e.parentElement.parentElement.firstChild.textContent);
+            //     e = e.parentElement.parentElement;
+            // }
+            // const text = textList.reverse().join("/");
+            // console.log(text);
+            const vpath = e.getAttribute("data-vpath")
+            console.log(vpath);
+            load_cheatsheet(vpath);
+        })
+    });
+    folderList.querySelectorAll(".folder").forEach(e => {
+    //e.click();
+    });
+    load_cheatsheet("ABOUT");
+}
 
 let isCtrlPressed = false;
 document.addEventListener("keydown", function(event) {if (event.ctrlKey) {isCtrlPressed = true;}});
@@ -398,5 +433,187 @@ document.addEventListener("click", function(event) {
     }
 });
 
+
+
+
+
+
+
+
+
+function removeSuffix(str, suffix) {
+  if (str.endsWith(suffix)) {
+    return str.slice(0, -suffix.length);
+  }
+  return str;
+}
+function removePrefix(str, prefix) {
+    return str.startsWith(prefix) ? str.slice(prefix.length) : str;
+}
+function removePrefixIgnoreCase(str, prefix) {
+    return str.toLowerCase().startsWith(prefix.toLowerCase()) ? str.slice(prefix.length) : str;
+}
+
+function processingBlockQuote(blockquote) {
+    if (blockquote.firstChild.textContent.toLowerCase().startsWith("[!note]\n")) {
+        blockquote.classList.add("blockquote-note");
+        // class="unselectable"
+        // textContent
+        blockquote.firstChild.innerHTML = `<p class="blockquote-alerts-p" style="color: #478be6;"><svg class="blockquote-alerts-svg" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/></svg>Note</p>` + removePrefixIgnoreCase(blockquote.firstChild.innerHTML, "[!note]\n");
+        blockquote.style.paddingBottom = "6px";
+        blockquote.style.marginTop = "12px";
+    } else if (blockquote.firstChild.textContent.toLowerCase().startsWith("[!tip]\n")) {
+        blockquote.classList.add("blockquote-tip");
+        blockquote.firstChild.innerHTML = `<p class="blockquote-alerts-p" style="color: #57ab5a;"><svg class="blockquote-alerts-svg" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path d="M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.751 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"/></svg>Tip</p>` + removePrefixIgnoreCase(blockquote.firstChild.innerHTML, "[!tip]\n");
+        blockquote.style.paddingBottom = "6px";
+        blockquote.style.marginTop = "12px";
+    } else if (blockquote.firstChild.textContent.toLowerCase().startsWith("[!important]\n")) {
+        blockquote.classList.add("blockquote-important");
+        blockquote.firstChild.innerHTML = `<p class="blockquote-alerts-p" style="color: #986ee2;"><svg class="blockquote-alerts-svg" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/></svg>Important</p>` + removePrefixIgnoreCase(blockquote.firstChild.innerHTML, "[!important]\n");
+        blockquote.style.paddingBottom = "6px";
+        blockquote.style.marginTop = "12px";
+    } else if (blockquote.firstChild.textContent.toLowerCase().startsWith("[!warning]\n")) {
+        blockquote.classList.add("blockquote-warning");
+        blockquote.firstChild.innerHTML = `<p class="blockquote-alerts-p" style="color: #c69026;"><svg class="blockquote-alerts-svg" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"/></svg>Warning</p>` + removePrefixIgnoreCase(blockquote.firstChild.innerHTML, "[!warning]\n");
+        blockquote.style.paddingBottom = "6px";
+        blockquote.style.marginTop = "12px";
+    } else if (blockquote.firstChild.textContent.toLowerCase().startsWith("[!caution]\n")) {
+        blockquote.classList.add("blockquote-caution");
+        blockquote.firstChild.innerHTML = `<p class="blockquote-alerts-p" style="color: #e5534b;"><svg class="blockquote-alerts-svg" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path d="M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/></svg>Caution</p>` + removePrefixIgnoreCase(blockquote.firstChild.innerHTML, "[!caution]\n");
+        blockquote.style.paddingBottom = "6px";
+        blockquote.style.marginTop = "12px";
+    }
+}
+
+function processingCheatSheet() {
+    cheatsheet_field.querySelectorAll("a").forEach(a => {
+        if (
+            a.target == ""
+            && a.getAttribute("href")[0] !== "#"
+            && a.getAttribute("href")[0] !== "?"
+        ) {
+            a.target="_blank";
+        }
+    });
+    cheatsheet_field.querySelectorAll("thead").forEach(thead => {
+        if (thead.textContent.trim() == "") {
+            thead.remove();
+        }
+    });
+    cheatsheet_field.querySelectorAll("blockquote").forEach(blockquote => {
+        blockquote.innerHTML = blockquote.innerHTML.replaceAll(">\n<", "><").replaceAll(">\n    <", "><").trim();
+        if (blockquote.textContent.length > 100) {
+            blockquote.setAttribute("expandable", "")
+        }
+        processingBlockQuote(blockquote);
+    });
+    h_elements = cheatsheet_field.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    h_elements.forEach(header => {
+        let id = header.textContent.trim()
+            .toLowerCase()
+            .replace(/\s+/g, "-")
+            // .replace(/[^\w\-]+/g, "")
+            .replace(/\-\-+/g, "-")
+            .replace(/^-+/, "")
+            .replace(/-+$/, "");
+        header.id = id;
+        //svg = `<svg style="width: .6em;height: .9em;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"></path></svg>`;
+        header.innerHTML = `<a class="anchor" href="#${id}">${header.innerHTML}</a>`;
+    });
+
+    return; // TODO
+    if (h_elements.length !== 0) {
+        // TODO cheatsheet_field.innerHTML += `<div id="h_list_button" state="off" class="control_button"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/></svg></div>`;
+        breadcrumbs_row.innerHTML += `<div id="h_list_button" state="off" class="icon_btn"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/></svg></div>`;
+        h_list = document.createElement("div");
+        h_list.id = "h_list";
+
+        // pre_element = document.createElement("pre");
+        // pre_element.innerHTML = `<span style="color: rgb(255, 0, 0);">H1</span>&nbsp;<span class="h_list_sel"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15 7-7 7 7"/></svg></span>`;
+        // pre_element.setAttribute("onclick", "cheatsheet_field.scrollTo(0, 0);");
+        // h_list.appendChild(pre_element);
+
+        min_header = Math.min(...Array.from(h_elements).map((header) => {return Number(header.tagName[1])}));
+
+        h_elements.forEach(header => {
+            h_num = Number(header.tagName[1]);
+            color = {
+                1: "rgb(255,   0,   0)",
+                2: "rgb(250, 115,   0)",
+                3: "rgb(255, 250,   0)",
+                4: "rgb(  0, 255,   0)",
+                5: "rgb(  0, 160, 245)",
+                6: "rgb(221,   0, 242)",
+            }[h_num];
+            indent = {
+                1: "",
+                2: "&nbsp;".substring((min_header * 6) - 6),
+                3: "&nbsp;&nbsp;".substring((min_header * 6) - 6),
+                4: "&nbsp;&nbsp;&nbsp;".substring((min_header * 6) - 6),
+                5: "&nbsp;&nbsp;&nbsp;&nbsp;".substring((min_header * 6) - 6),
+                6: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".substring((min_header * 6) - 6),
+            }[h_num];
+            pre_element = document.createElement("pre");
+            //style = header.id === getAnchor() ? `background-color: rgb(75, 75, 75)` : ""; // style="${style}"
+            pre_element.innerHTML = `${indent}<span style="color: ${color}">${header.tagName}</span>&nbsp;<span class="h_list_sel">${header.textContent}</span>`;
+            pre_element.setAttribute("onclick", `document.getElementById("${header.id}").scrollIntoView({block: "start"})`);
+            h_list.appendChild(pre_element);
+        });
+        cheatsheet_field.appendChild(h_list);
+
+        function f1 () {
+            // h_list_button.style.borderTopLeftRadius = "0";
+            // h_list_button.style.borderBottomLeftRadius = "0";
+            // h_list_button.style.backgroundColor = "rgb(25, 25, 25)";
+            h_list.style.display = "block";
+            // h_list.style.borderTopRightRadius = "0";
+        }
+        function f2 () {
+            // h_list_button.style.borderTopLeftRadius = "";
+            // h_list_button.style.borderBottomLeftRadius = "";
+            // h_list_button.style.backgroundColor = "#525252";
+            h_list.style.display = "none";
+            // h_list.style.borderBottomRightRadius = "";
+        }
+        function f3 () {
+            if (h_list_button.getAttribute("state") === "on") {
+                h_list_button.addEventListener("mouseover", f1);
+                h_list_button.addEventListener("mouseout", f2);
+                h_list       .addEventListener("mouseover", f1);
+                h_list       .addEventListener("mouseout", f2);
+
+                h_list_button.addEventListener("touchstart", f1);
+                h_list_button.addEventListener("touchend", f2);
+                h_list       .addEventListener("touchstart", f1);
+                h_list       .addEventListener("touchend", f2);
+
+                h_list_button.setAttribute("state", "off");
+            } else {
+                // h_list_button.style.borderTopLeftRadius = "0";
+                // h_list_button.style.borderBottomLeftRadius = "0";
+                // h_list_button.style.backgroundColor = "rgb(25, 25, 25)";
+                h_list.style.display = "block";
+                // h_list.style.borderTopRightRadius = "0";
+
+                h_list_button.removeEventListener("mouseover", f1);
+                h_list_button.removeEventListener("mouseout", f2);
+                h_list       .removeEventListener("mouseover", f1);
+                h_list       .removeEventListener("mouseout", f2);
+
+                h_list_button.removeEventListener("touchstart", f1);
+                h_list_button.removeEventListener("touchend", f2);
+                h_list       .removeEventListener("touchstart", f1);
+                h_list       .removeEventListener("touchend", f2);
+
+                h_list_button.setAttribute("state", "on");
+            }
+        }
+        h_list_button.addEventListener("mouseover", f1);
+        h_list_button.addEventListener("mouseout", f2);
+        h_list.addEventListener("mouseover", f1);
+        h_list.addEventListener("mouseout", f2);
+        h_list_button.addEventListener("click", f3);
+    }
+};
 
 
