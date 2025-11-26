b9b679ddf3645020d2beb593e82dc6b304163d32
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-11-27 02:55:57 +0400

    updated

--- _dev/markup.py
+++ _dev/markup.py
@@ -53,6 +53,9 @@ def code_block_callback(match: re.Match) -> str:
     ):
         highlighted_code = set_unselectable(highlighted_code, "\n")
 
+    regex_comma_color_span = re.compile(r'<span class="p">,</span>')
+
+    highlighted_code = regex_comma_color_span.sub(lambda m: '<span class="k">,</span>', highlighted_code)
     highlighted_code = highlighted_code.replace("`", "&#x60;")
     copy_svg = '<svg style="width: 1.2em;height: 1.2em;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-5-4v4h4V3h-4Z"/></svg>'
     download_svg = '<svg style="width: 1.2em;height: 1.2em;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 0 1-1 1H5m5 4-2 2 2 2m4-4 2 2-2 2m5-12v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Z"/></svg>'
