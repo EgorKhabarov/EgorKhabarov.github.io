3fdc480ed365d6a88849b6480809ff455992c61e
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2025-11-27 21:20:16 +0400

    updated

--- cheatsheet/scripts.js
+++ cheatsheet/scripts.js
@@ -358,8 +358,8 @@ window.addEventListener("resize", () => {
     } else {
         sidebar.classList.remove("display_none");
         resizer.classList.remove("display_none");
-        sidebar_open_button.classList.remove("display_none");
-        sidebar_close_button.classList.remove("display_none");
+        // sidebar_open_button.classList.remove("display_none");
+        // sidebar_close_button.classList.remove("display_none");
         // sidebar.classList.toggle('display_none');resizer.classList.toggle('display_none');
     }
 });
@@ -887,7 +887,7 @@ function processingCheatSheet() {
     });
     cheatsheet_field.querySelectorAll("table").forEach(processingTables);
 
-    //return; // TODO
+    return; // TODO
     if (h_elements.length !== 0) {
         // TODO cheatsheet_field.innerHTML += `<div id="h_list_button" state="off" class="control_button"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/></svg></div>`;
 
